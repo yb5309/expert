df2ce9118111a0adcad83eca5530a97cfd9a1702
雷神之手 <vip-yang@163.com>
2019-12-17 09:23:22 +0800

    Update ExpertMain.java

--- src/main/java/ExpertMain.java
+++ src/main/java/ExpertMain.java
@@ -4,7 +4,7 @@ public class ExpertMain {
 
 	public static void main(String[] args) {
 		System.out.println(add(1, 2));
-		System.out.println(add(3, 2));
+		System.out.println(add(2, 2));
 	}
 	
 	public static int add(int a, int b) {
